2f0797535e539afac5da9f4d8b3661179a8c472e
carlos castillo blas <carloscacb333@gmail.com>
2023-09-18 17:27:04 -0500

    change docker configuration

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("🚀 Starting server...")
-	if os.Getenv("debug") == "release" {
+	fmt.Println("🚀 Starting server on port: " + os.Getenv("PORT"))
+	if os.Getenv("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
 		InitMigrations()
+		gin.SetMode(gin.DebugMode)
 	}
 	r := SetupRouter()
 	r.Run(":" + os.Getenv("PORT"))
